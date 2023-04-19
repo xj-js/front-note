a41e17bbaa9bbddb6c158c7e850fe1a94c37c879
lixiaojun <lixiaojun@trip.com>
2023-04-19 15:03:47 +0800

    feat: js基础知识

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,8 +1,14 @@
 export default {
 	dest: 'public',
-	base: '/open-source/car-front-standard/',
+	base: '/xj-js/front-note/',
 	title: '前端学习笔记',
 	themeConfig: {
-		nav: [{ text: 'Guide', link: '/index' }]
+		nav: [{ text: 'Guide', link: '/index' }],
+		sidebar: [
+			{
+				text: 'JavaScript基础知识的简要概述',
+				link: '/js-base/index',
+			}
+		],
 	},
 };
